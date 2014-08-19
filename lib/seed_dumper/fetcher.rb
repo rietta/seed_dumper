96d460ab489edc15946d28e937506c13f665ced4
Frank Rietta <frank@rietta.com>
2014-08-19 02:14:07 -0400

    Skip modules...

--- lib/seed_dumper/fetcher.rb
+++ lib/seed_dumper/fetcher.rb
@@ -11,36 +11,39 @@ module SeedDumper
 
       puts "Adding #{model_name.camelize} seeds."
 
-      records = klass.all.map do |record|
-        attr_s = [];
+      if klass and klass.respond_to?(:all)
+        # Some objects, such as modules will not be scannable like this
+        records = klass.all.map do |record|
+          attr_s = [];
 
-        record.attributes.delete_if { |k, v| ignore.include?(k) }.each do |key, value|
-          vc = value.class
+          record.attributes.delete_if { |k, v| ignore.include?(k) }.each do |key, value|
+            vc = value.class
 
-          value = value.class == Time ? "\"#{value}\"" : value.inspect
-          value = nil if value.is_a?(String) && value == "\"\""
-          value = nil if value == 'nil' || value == "nil"
+            value = value.class == Time ? "\"#{value}\"" : value.inspect
+            value = nil if value.is_a?(String) && value == "\"\""
+            value = nil if value == 'nil' || value == "nil"
 
-          if not value.nil?
-            if vc == DateTime or ('created_at' == key or 'updated_at' == key)
-              attr_s.push("#{key.to_sym.inspect} => DateTime.parse('#{value}')")
-            elsif vc == Date
-              attr_s.push("#{key.to_sym.inspect} => Date.parse('#{value}')")
-            elsif vc == Time
-              attr_s.push("#{key.to_sym.inspect} => Time.parse('#{value}')")
-            else
-              attr_s.push("#{key.to_sym.inspect} => #{value}")# unless key == 'id'
+            if not value.nil?
+              if vc == DateTime or ('created_at' == key or 'updated_at' == key)
+                attr_s.push("#{key.to_sym.inspect} => DateTime.parse('#{value}')")
+              elsif vc == Date
+                attr_s.push("#{key.to_sym.inspect} => Date.parse('#{value}')")
+              elsif vc == Time
+                attr_s.push("#{key.to_sym.inspect} => Time.parse('#{value}')")
+              else
+                attr_s.push("#{key.to_sym.inspect} => #{value}")# unless key == 'id'
+              end
             end
+
+
           end
 
-
+          record_dump = "#{model_name.camelize}.create(#{attr_s.join(', ')})"
+          record_dump = "#{record_dump}{|record| record.id = #{record.attributes['id']}}" if options[:dump_id] && record.attributes['id']
+          record_dump
         end
-
-        record_dump = "#{model_name.camelize}.create(#{attr_s.join(', ')})"
-        record_dump = "#{record_dump}{|record| record.id = #{record.attributes['id']}}" if options[:dump_id] && record.attributes['id']
-        record_dump
+        # / records.each_with_index
       end
-      # / records.each_with_index
 
       records
     end
